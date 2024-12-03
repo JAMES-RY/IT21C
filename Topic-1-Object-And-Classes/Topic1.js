b410c7bd9d57bc36ba58261f58723236fe8fd08b
JAMES-RY <jamesrymerbagasbasocero@gmail.com>
2024-12-03 19:01:58 +0800

    All Commit

---
+++ Topic-1-Object-And-Classes/Topic1.js
@@ -0,0 +1,17 @@
+
+        //Input
+
+        const person1 = {
+            name:"James",
+            age:20,
+            status:" HEHEH"
+}
+
+       //Process 
+
+        //Display the Person Attribute in the Console
+        console.log(" Name:" + person1.name);
+        console.log(" Age:" + person1.age);
+        console.log(" Name:" + person1.status);
+
+   
